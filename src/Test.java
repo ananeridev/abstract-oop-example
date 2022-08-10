50c470e5e9dd1f5aacbf5ab6a29b0c583a80bb3b
anabneri <neribia7@gmail.com>
2022-08-10 20:03:10 -0300

    initial commit
    Signed-off-by: anabneri <neribia7@gmail.com>

---
+++ src/Test.java
@@ -0,0 +1,74 @@
+abstract class Formato {
+
+    String cor;
+
+    // these are abstract methods
+    abstract double area();
+    public abstract String toString();
+
+    // abstract class can have the constructor
+    public Formato(String cor)
+    {
+        System.out.println("Formato construtor é chamado");
+        this.cor = cor;
+    }
+
+    // this is a concrete method
+    public String getCor() { return cor; }
+}
+class Circulo extends Formato {
+    double raio;
+
+    public Circulo(String cor, double raio)
+    {
+
+        // calling Shape constructor
+        super(cor);
+        System.out.println("Construtor circulo é chamado");
+        this.raio = raio;
+    }
+
+    @Override double area()
+    {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    @Override public String toString()
+    {
+        return "Cor do circulo é " + super.getCor()
+                + "e a área é : " + area();
+    }
+}
+class Retangulo extends Formato {
+
+    double comprimento;
+    double largura;
+
+    public Retangulo(String cor, double comprimento,
+                     double largura)
+    {
+        // calling Shape constructor
+        super(cor);
+        System.out.println("Construtor do retangulo é chamado");
+        this.comprimento = comprimento;
+        this.largura = largura;
+    }
+
+    @Override double area() { return comprimento * largura; }
+
+    @Override public String toString()
+    {
+        return "Cor do ratangulo é " + super.getCor()
+                + "e a área é : " + area();
+    }
+}
+public class Test {
+    public static void main(String[] args)
+    {
+        Formato f1 = new Circulo("Vermelho", 2.2);
+        Formato f2 = new Retangulo("Amarelo", 2, 4);
+
+        System.out.println(f1.toString());
+        System.out.println(f2.toString());
+    }
+}
